5896d511c323cf1d3691e782748f6e08c11ffef4
Paka <66157320+vjh0107@users.noreply.github.com>
2020-12-21 06:35:50 +0900

    Add files via upload

---
+++ mqttSubscribe/mqttSubscribe.ino
@@ -0,0 +1,109 @@
+
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+#include <FirebaseCloudMessaging.h>
+#include <Firebase.h>
+#include <FirebaseHttpClient.h>
+#include <FirebaseArduino.h>
+#include <FirebaseError.h>
+#include <FirebaseObject.h>
+
+// Update these with values suitable for your network.
+
+#define FIREBASE_HOST "kjsw-a888e.firebaseio.com"
+#define FIREBASE_AUTH "iCNC1KqXIvPtY62s7D5QnN7gfKc5hmNUow9Er0Ye"
+
+const char* ssid = "ParkJunhyung_Personal";
+const char* password = "!vjh1216";
+const char* mqtt_server = "192.168.1.2";
+
+
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+
+void setup() {
+  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.reconnectWiFi(true);
+ 
+  firebaseData.setBSSLBufferSize(1024, 1024);
+  firebaseData.setResponseSize(1024);
+  Firebase.setReadTimeout(firebaseData, 1000 * 60);
+  Firebase.setwriteSizeLimit(firebaseData, "tiny");
+}
+
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == '1') {
+    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
+    // but actually the LED is on; this is because
+    // it is acive low on the ESP-01)
+  } else {
+    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
+  }
+
+}
+
+void wifiReconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP8266Client")) {
+      Serial.println("connected");
+
+      // ... and resubscribe
+      client.subscribe("hello");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+void loop() {
+  if (!client.connected()) {
+    wifiReconnect();
+  }
+  client.loop();
+}
